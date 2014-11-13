7ad6dc7713d58a48d90b60ec14fad34d55770101
Andy Durant <andy@ajdurant.co.uk>
2014-11-13 15:07:39 +0000

    navbar login

--- models/db.py
+++ models/db.py
@@ -25,8 +25,31 @@ from gluon.tools import Auth
 
 auth = Auth(db)
 
+db.define_table(
+    auth.settings.table_user_name,
+    Field('username', length=128),
+    Field('realname', length=128, default='', label='Name'),
+    Field('birthdate', 'date', required=True),
+    Field('email', length=128, default='', unique=True),                                    # required
+    Field('password', 'password', length=512, readable=False, label='Password'),            # required
+    Field('registration_key', length=512, writable=False, readable=False, default=''),      # required
+    Field('reset_password_key', length=512, writable=False, readable=False, default=''),    # required
+    Field('registration_id', length=512, writable=False, readable=False, default=''),       # required
+    format='%(realname)s (%(username)s)')
+
+## do not forget validators
+custom_auth_table = db[auth.settings.table_user_name] # get the custom_auth_table
+custom_auth_table.username.requires = [IS_NOT_EMPTY(error_message=auth.messages.is_empty), IS_NOT_IN_DB(db, custom_auth_table.username)]
+custom_auth_table.realname.requires = IS_NOT_EMPTY(error_message=auth.messages.is_empty)
+custom_auth_table.password.requires = [IS_STRONG(), CRYPT()]
+custom_auth_table.email.requires = [
+    IS_EMAIL(error_message=auth.messages.invalid_email),
+    IS_NOT_IN_DB(db, custom_auth_table.email)]
+
+auth.settings.table_user = custom_auth_table # tell auth to use custom_auth_table
+
 ## create all tables needed by auth if not custom tables
-auth.define_tables(username=False, signature=False)
+auth.define_tables(username=True, signature=False)
 
 
 ## configure auth policy
--- models/db_bootup.py
+++ models/db_bootup.py
@@ -1,14 +1,6 @@
 
-# DB Table for BootUp Users
-db.define_table(
-    'person',
-    Field('uname', unique=True, required=True),
-    Field('realname', required=True),
-    Field('birthdate', 'date', required=True),
-    format='%(realname)s (%(uname)s)'
-)
+# DB Table for BootUp Users in db.py for auth
 
-db.person.uname.requires = IS_NOT_IN_DB(db, db.person.uname)
 
 # Lookup table for categories
 db.define_table(
@@ -35,16 +27,16 @@ db.define_table(
     Field('sdesc', 'string', length=120, required=True),
     Field('ldesc', 'text', required=True),
     Field('story', 'text', required=True),
-    Field('manager', 'reference person', required=True),
+    Field('manager', 'reference auth_user', writable=False, readable=False, required=True),
     format='%(title)s'
 )
 
-db.project.manager.requires = IS_IN_DB(db, db.person.id, '%(uname)s')
+db.project.manager.requires = IS_IN_DB(db, db.auth_user.id, '%(uname)s')
 
 # Lookup table for project rewards
 db.define_table(
     'reward',
-    Field('projectid', 'reference project', required=True),
+    Field('projectid', 'reference project', writable=False, readable=False, required=True),
     Field('amount', 'integer', required=True),
     Field('reward', required=True)
 )
@@ -52,26 +44,27 @@ db.define_table(
 # Lookup table for project pledges
 db.define_table(
     'pledge',
-    Field('projectid', 'reference project', required=True),
-    Field('uname', 'reference person', required=True),
+    Field('projectid', 'reference project', writable=False, readable=False, required=True),
+    Field('username', 'reference auth_user', required=True),
     Field('amount', 'integer', required=True)
 )
 
 # DB Table for User Addresses
 db.define_table(
     'address',
-    Field('uname', 'reference person', required=True),
+    Field('username', 'reference auth_user', writable=False, readable=False, required=True),
     Field('street', required=True),
     Field('city', required=True),
     Field('country', required=True),
     Field('post1', required=True),
-    Field('post2', required=True)
+    Field('post2', required=True),
+    format='%(street)s'
 )
 
 # DB Table for User Credit Cards
 db.define_table(
     'cc',
-    Field('uname', 'reference person', required=True),
+    Field('username', 'reference auth_user', writable=False, readable=False, required=True),
     Field('address', 'reference address', required=True),
     Field('ccnum', required=True),
     Field('expires', 'date', required=True),
